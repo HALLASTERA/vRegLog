cf5c19f640f1de8acf02640bf1c516d3c7434873
hallastera <hallastera@gmail.com>
2018-07-08 13:23:45 +0300

    version 1

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -5,14 +5,17 @@ from django.http import HttpResponse
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
 from django.contrib.auth.models import User
+
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-
+@login_required
 def home(request):
     numbers = [1,2,3,4,5]
     name = 'Anton Sergienko'
     args = {'name': name, 'numbers': numbers}
     return render(request, 'accounts/home.html', args)
+
 
 def register(request):
     if request.method=='POST':
@@ -26,10 +29,11 @@ def register(request):
         args = {'form': form}
         return render(request, 'accounts/reg_form.html', args)
 
+@login_required
 def view_profile(request):
     args = {'user': request.user}
     return render(request, 'accounts/profile.html', args)
-
+@login_required
 def edit_profile(request):
     if request.method=='POST':
         form = EditProfileForm(request.POST, instance=request.user)
@@ -42,7 +46,7 @@ def edit_profile(request):
         return render(request, 'accounts/edit_profile.html', args)
 
 
-
+@login_required
 def change_password(request):
     if request.method=='POST':
         form = PasswordChangeForm(data=request.POST, user=request.user)
