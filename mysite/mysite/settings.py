7b3bc6150f4c731da4447cfcc87bc75a266a67e2
Anton Sergienko <hallastera@gmail.com>
2018-07-25 18:23:36 +0300

    version photoID

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -118,5 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mysite/media')
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = '/account/'
